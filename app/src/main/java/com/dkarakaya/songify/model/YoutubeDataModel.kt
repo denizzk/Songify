89263467ef54b1996ca11bc12b01a88b7e6a9df7
denizkarakaya <deniz.karakaya@traderepublic.com>
2020-04-13 18:29:34 +0200

    refactor youtube data model

--- app/src/main/java/com/dkarakaya/songify/model/YoutubeDataModel.kt
+++ app/src/main/java/com/dkarakaya/songify/model/YoutubeDataModel.kt
@@ -1,48 +1,11 @@
 package com.dkarakaya.songify.model
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
-data class YoutubeDataModel(var title: String? = "",
-                            var channelTitle: String? = "",
-                            var publishedAt: String? = "",
-                            var thumbnail: String? = "",
-                            var videoId: String? = "") : Parcelable {
-
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(title)
-        dest.writeString(channelTitle)
-        dest.writeString(publishedAt)
-        dest.writeString(thumbnail)
-        dest.writeString(videoId)
-    }
-
-    private constructor(parcel: Parcel) : this() {
-        readFromParcel(parcel)
-    }
-
-    private fun readFromParcel(parcel: Parcel) {
-        title = parcel.readString()
-        channelTitle = parcel.readString()
-        publishedAt = parcel.readString()
-        thumbnail = parcel.readString()
-        videoId = parcel.readString()
-    }
-
-    companion object {
-        val CREATOR: Parcelable.Creator<YoutubeDataModel> = object : Parcelable.Creator<YoutubeDataModel> {
-            override fun createFromParcel(parcel: Parcel): YoutubeDataModel? {
-                return YoutubeDataModel(parcel)
-            }
-
-            override fun newArray(size: Int): Array<YoutubeDataModel?> {
-                return arrayOfNulls(size)
-            }
-        }
-    }
-}
+@Parcelize
+data class YoutubeDataModel(var title: String?,
+                            var channelTitle: String?,
+                            var publishedAt: String?,
+                            var thumbnail: String?,
+                            var videoId: String?) : Parcelable
